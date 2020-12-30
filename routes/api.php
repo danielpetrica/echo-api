842db179315bfbac67d075c46a17346d7c09c205
Daniel, Andrei-Daniel Petrica <andrei@danielpetrica.com>
2020-12-30 10:36:45 +0100

    api test

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function(Request $request) {
+    return "Responding from a vercel.com lambda built with Laravel 👋🏻";
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
