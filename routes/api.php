647c44837121485ae0dbc2d79cbc5395fab77553
Andrei Daniel Petrica <daniel@danielpetrica.pw>
2020-12-31 18:23:39 +0100

    minor change

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::any('/{any}', function(Request $request) {
-    return $request->getContent();
+    return $request->all();
 })->where('any', '.*');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
